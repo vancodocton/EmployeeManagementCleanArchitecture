92d58e74cd948567b2cbf0267a06ddfddba20030
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-06-03 10:56:59 +0700

    Create SysEmployee Entity and Repository

---
+++ src/Employee.Core/Entities/SysEmployee.cs
@@ -0,0 +1,33 @@
+﻿using Employee.Core.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee.Core.Entities
+{
+    public class SysEmployee : IEntity<int>
+    {
+        public int Id { get; set ; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.EmailAddress), StringLength(100)]
+        public string Email { get; set; }
+
+        public SysEmployee()
+        {
+            FirstName = null!;
+            LastName = null!;
+            DateOfBirth = default;
+            PhoneNumber = null!;
+            Email = null!;
+        }
+    }
+}
---
+++ src/Employee.Core/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Employee.Core.Entities;
+using Employee.Core.Interfaces;
+
+namespace Employee.Core.Repositories
+{
+    public interface IEmployeeRepository : IRepository<SysEmployee>
+    {
+        Task<IEnumerable<SysEmployee>> GetEmployeeByLastNameAsync(string lastname);
+    }
+}
