82f629b31ca81cc7009adfe04671888a3c31785f
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-06-03 11:28:05 +0700

    Create Employee Repository

---
+++ src/Employee.Infrastructure/Data/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Employee.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Infrastructure.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<SysEmployee> Employees { get; set; }
+
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+            Employees = Set<SysEmployee>();
+        }
+    }
+}
---
+++ src/Employee.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using Employee.Core.Entities;
+using Employee.Core.Repositories;
+using Employee.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Infrastructure.Repositories
+{
+    internal class EmployeeRepository : RepositoryBase<SysEmployee>, IEmployeeRepository
+    {
+        private readonly DbSet<SysEmployee> employees;
+
+        public EmployeeRepository(EmployeeDbContext dbContext) : base(dbContext)
+        {
+            employees = dbContext.Set<SysEmployee>();
+        }
+
+        public async Task<IEnumerable<SysEmployee>> GetEmployeeByLastNameAsync(string lastname)
+        {
+            return await employees.Where(m => m.LastName == lastname).ToListAsync();
+        }
+    }
+}
